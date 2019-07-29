c70ab0a6e1d718006fcbf3bd10b2549c53667e78
duyu <dydeve@163.com>
2019-07-29 18:12:08 +0800

    add run time

--- com/py/dl/tf_offical_tutorials/eager/eager_basics.py
+++ com/py/dl/tf_offical_tutorials/eager/eager_basics.py
@@ -94,11 +94,11 @@ print("On CPU:")
 with tf.device("CPU:0"):
     x = tf.random_uniform([1000, 1000])
     assert x.device.endswith("CPU:0")
-    time_matmul(x)
+    time_matmul(x)  # 10 loops: 290.22ms
 
 # Force execution on GPU #0 if available
 if tf.test.is_gpu_available():
     with tf.device("GPU:0"):  # Or GPU:1 for the 2nd GPU, GPU:2 for the 3rd etc.
         x = tf.random_uniform([1000, 1000])
         assert x.device.endswith("GPU:0")
-        time_matmul(x)
+        time_matmul(x)  # 10 loops: 237.37ms
