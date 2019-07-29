b27d632a346de4a2ca47331b92c8126106b23ca2
joker <joker.du@ximalaya.com>
2019-07-29 17:48:33 +0800

    eager_basic

--- com/py/dl/tf_offical_tutorials/1index.md
+++ com/py/dl/tf_offical_tutorials/1index.md
@@ -9,3 +9,16 @@ https://www.tensorflow.org/tutorials
     - [回归 / Regression](./learn_and_use_ml/basic_regression.py)
     - [过拟合与欠拟合 / Overfitting and underfitting](./learn_and_use_ml/overfit_and_underfit.py)
     - [保存并恢复模型 / Save and restore models](./learn_and_use_ml/save_and_retore_models.py)
+- [研究和实验]
+
+    Eager execution provides an imperative, define-by-run interface for advanced operations. Write custom layers, forward passes, and training loops with auto differentiation.
+    
+    通过自动微分写自定义层、前向传播、循环训练
+
+    - [Eager execution](./eager/eager_basics.py)
+    - [自动微分和梯度记录 / Automatic differentiation and gradient tape]
+    - [自定义训练：基础知识 / Custom training: basics]
+    - [自定义层 / Custom layers]
+    - [自定义训练：演示 / Custom training: walkthrough]
+    - [高级示例：基于注意力的神经机器翻译]
+
---
+++ com/py/dl/tf_offical_tutorials/eager/eager_basics.py
@@ -0,0 +1,87 @@
+from __future__ import absolute_import, division, print_function
+
+import tensorflow as tf
+import numpy as np
+
+"""
+  Eager execution provides an imperative interface to TensorFlow. With eager
+  execution enabled, TensorFlow functions execute operations immediately (as
+  opposed to adding to a graph to be executed later in a `tf.Session`) and
+  return concrete values (as opposed to symbolic references to a node in a
+  computational graph).
+"""
+tf.enable_eager_execution()
+
+"""
+A Tensor is a multi-dimensional array. Similar to NumPy ndarray objects, Tensor objects have a data type and a shape. 
+Additionally, Tensors can reside in accelerator (like GPU) memory. 
+TensorFlow offers a rich library of operations (tf.add, tf.matmul, tf.linalg.inv etc.) that consume and produce Tensors. 
+These operations automatically convert native Python types.
+"""
+print(tf.add(1, 2))  # tf.Tensor(3, shape=(), dtype=int32)
+print(tf.add([1, 2], [3, 4]))  # tf.Tensor([4 6], shape=(2,), dtype=int32)
+print(tf.square(5))  # tf.Tensor(25, shape=(), dtype=int32)
+print(tf.reduce_sum([1, 2, 3]))  # tf.Tensor(6, shape=(), dtype=int32)
+print(tf.encode_base64("hello world"))  # tf.Tensor(b'aGVsbG8gd29ybGQ', shape=(), dtype=string)
+
+# Operator overloading is also supported
+print(tf.square(2) + tf.square(3))  # tf.Tensor(13, shape=(), dtype=int32)
+
+# Each Tensor has a shape and a datatype
+x = tf.matmul([[1]], [[2, 3]])
+print(x.shape)
+print(x.dtype)
+
+# The most obvious differences between NumPy arrays and TensorFlow Tensors are:
+#   1. Tensors can be backed by accelerator memory (like GPU, TPU).
+#   2. Tensors are immutable.
+
+"""
+Tensors can be explicitly converted to NumPy ndarrays by invoking the .numpy() method on them. 
+These conversions are typically cheap as the array and Tensor share the underlying memory representation if possible. 
+However, sharing the underlying representation isn't always possible since the Tensor may be hosted in GPU memory 
+while NumPy arrays are always backed by host memory, and the conversion will thus involve a copy from GPU to host memory.
+"""
+
+
+ndarray = np.ones([3, 3])
+
+print("TensorFlow operations convert numpy arrays to Tensors automatically")
+tensor = tf.multiply(ndarray, 42)
+print(tensor)
+
+
+print("And NumPy operations convert Tensors to numpy arrays automatically")
+print(np.add(tensor, 1))
+
+print("The .numpy() method explicitly converts a Tensor to a numpy array")
+print(tensor.numpy())
+
+
+"""
+Many TensorFlow operations can be accelerated by using the GPU for computation. 
+Without any annotations, TensorFlow automatically decides whether to use the 
+GPU or CPU for an operation (and copies the tensor between CPU and GPU memory if necessary). 
+Tensors produced by an operation are typically backed by the memory of the device on which the operation executed.
+"""
+
+x = tf.random_uniform([3, 3])
+
+print("Is there a GPU available: "),
+print(tf.test.is_gpu_available())
+
+print("Is the Tensor on GPU #0:  "),
+print(x.device.endswith('GPU:0'))
+
+
+
+
+
+
+
+
+
+
+
+
+
