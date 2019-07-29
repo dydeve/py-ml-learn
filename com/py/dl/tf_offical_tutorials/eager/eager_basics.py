b42241766c63755d24c70c7fab2cd13da0ff11ad
joker <joker.du@ximalaya.com>
2019-07-29 18:00:32 +0800

    run gpu

--- com/py/dl/tf_offical_tutorials/eager/eager_basics.py
+++ com/py/dl/tf_offical_tutorials/eager/eager_basics.py
@@ -2,6 +2,7 @@ from __future__ import absolute_import, division, print_function
 
 import tensorflow as tf
 import numpy as np
+import time
 
 """
   Eager execution provides an imperative interface to TensorFlow. With eager
@@ -43,20 +44,17 @@ However, sharing the underlying representation isn't always possible since the T
 while NumPy arrays are always backed by host memory, and the conversion will thus involve a copy from GPU to host memory.
 """
 
-
 ndarray = np.ones([3, 3])
 
 print("TensorFlow operations convert numpy arrays to Tensors automatically")
 tensor = tf.multiply(ndarray, 42)
 print(tensor)
 
-
 print("And NumPy operations convert Tensors to numpy arrays automatically")
 print(np.add(tensor, 1))
 
 print("The .numpy() method explicitly converts a Tensor to a numpy array")
 print(tensor.numpy())
-
 
 """
 Many TensorFlow operations can be accelerated by using the GPU for computation. 
@@ -67,6 +65,7 @@ Tensors produced by an operation are typically backed by the memory of the devic
 
 x = tf.random_uniform([3, 3])
 
+# depends on nvidia gpu or tensorflow-gpu
 print("Is there a GPU available: "),
 print(tf.test.is_gpu_available())
 
@@ -74,14 +73,32 @@ print("Is the Tensor on GPU #0:  "),
 print(x.device.endswith('GPU:0'))
 
 
+"""
+TensorFlow automatically decides which device to execute an operation, and copies Tensors to that device if needed. 
+However, TensorFlow operations can be explicitly placed on specific devices using the tf.device context manager.
+"""
 
 
+def time_matmul(x):
+    start = time.time()
+    for loop in range(10):
+        tf.matmul(x, x)
+
+    result = time.time() - start
+
+    print("10 loops: {:0.2f}ms".format(1000 * result))
 
 
+# Force execution on CPU
+print("On CPU:")
+with tf.device("CPU:0"):
+    x = tf.random_uniform([1000, 1000])
+    assert x.device.endswith("CPU:0")
+    time_matmul(x)
 
-
-
-
-
-
-
+# Force execution on GPU #0 if available
+if tf.test.is_gpu_available():
+    with tf.device("GPU:0"):  # Or GPU:1 for the 2nd GPU, GPU:2 for the 3rd etc.
+        x = tf.random_uniform([1000, 1000])
+        assert x.device.endswith("GPU:0")
+        time_matmul(x)
